413f4fe19d33ee9d62634429683db1943ab35599
SDS <sms5217409@gmail.com>
2025-09-01 12:03:50 +0900

    login

--- app/src/main/java/com/example/devmour/MainActivity2.kt
+++ app/src/main/java/com/example/devmour/MainActivity2.kt
@@ -30,7 +30,7 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
